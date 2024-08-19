fd20adf7366be9599fd7458853cab7965944a752
woori <woori.dec@gmail.com>
2024-08-19 16:02:11 +0900

    first commit

---
+++ src/main/java/com/newneek/NewneekApplication.java
@@ -0,0 +1,13 @@
+package com.newneek;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewneekApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewneekApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/newneek/NewneekApplicationTests.java
@@ -0,0 +1,13 @@
+package com.newneek;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewneekApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
